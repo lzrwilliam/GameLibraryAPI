6be3e985636350d86b52c40ac13f116eaab1eb7c
IonescuMihaiLeonard <92025038+IonescuMihaiLeonard@users.noreply.github.com>
2025-03-06 12:57:36 +0000

    [USERS]Add+GetAll+DeleteAll

---
+++ src/main/java/ro/unibuc/hello/data/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package ro.unibuc.hello.data.repository;
+
+import ro.unibuc.hello.data.model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+}
---
+++ src/main/java/ro/unibuc/hello/service/UserService.java
@@ -0,0 +1,31 @@
+package ro.unibuc.hello.service;
+
+import ro.unibuc.hello.data.model.User;
+import ro.unibuc.hello.data.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.io.PrintStream;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService{
+    private final UserRepository _userRepository;
+
+    public UserService(UserRepository userRepository) {
+        _userRepository = userRepository;
+    }
+
+    public List<User> getAllUsers(){
+        return _userRepository.findAll();
+    }
+
+    public User addUser(User user){
+        return _userRepository.save(user);
+        
+    }
+
+    public void deleteAllUsers() {
+        _userRepository.deleteAll();
+    }
+}
