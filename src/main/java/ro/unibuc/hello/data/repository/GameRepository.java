9d8147f85ae04a0c96f388cc71fe3182e07393bd
IonescuMihaiLeonard <92025038+IonescuMihaiLeonard@users.noreply.github.com>
2025-03-16 16:42:39 +0000

    [GAMES] Search by genre

--- src/main/java/ro/unibuc/hello/data/repository/GameRepository.java
+++ src/main/java/ro/unibuc/hello/data/repository/GameRepository.java
@@ -4,6 +4,10 @@ import ro.unibuc.hello.data.model.Game;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+
 @Repository
 public interface GameRepository extends MongoRepository<Game, Integer> {
+    List<Game> findByGenre(String genre);
 }
